c6bb320a60293bc7a48050930cad9c33f851f73e
shibowe <dev@winecos.com>
2021-06-09 21:01:37 +0800

    add feature of xch

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 )
@@ -11,7 +14,7 @@ type NetSpace struct {
 	Success   bool    `json:"success"`
 	DayChange float64 `json:"daychange"`
 	Netspace  int64   `json:"netspace"`
-	Timestamp int64   `json:"timestamp"`
+	Timestamp string  `json:"timestamp"`
 }
 type Market struct {
 	Success   bool    `json:"success"`
@@ -19,54 +22,80 @@ type Market struct {
 	Daymin    float64 `json:"daymin"`
 	Daymax    float64 `json:"daymax"`
 	Daychange float64 `json:"daychange"`
-	Timestamp int64   `json:"timestamp"`
+	Timestamp string  `json:"timestamp"`
+}
+type XCH struct {
+	Netspace  string
+	Price     string
+	Daychange string
+}
+type Notify struct {
+	Msgtype  string `json:"msgtype"`
+	Markdown struct {
+		Content string `json:"content"`
+	} `json:"markdown"`
 }
 
+var httpClient = &http.Client{Timeout: 10 * time.Second}
+
 func main() {
-	fmt.Println("Hello,Go!")
+	ns := new(NetSpace)
+	market := new(Market)
+
 	netspaceURL := "https://api.chiaprofitability.com/netspace"
 	xchURL := "https://api.chiaprofitability.com/market"
 
-	ReqNetspaceByURL(netspaceURL)
-	time.Sleep(2 * time.Second)
-	ReqMarketByURL(xchURL)
+	getJson(netspaceURL, ns)
+	fmt.Println(ns.Netspace)
+	fmt.Println(ns.DayChange)
+
+	getJson(xchURL, market)
+	fmt.Println(market.Price)
+
+	//Post
+	xch := new(XCH)
+	xch.Daychange = fmt.Sprintf("%f", ns.DayChange)
+	xch.Netspace = fmt.Sprintf("%d", ns.Netspace)
+	xch.Price = fmt.Sprintf("%f", market.Price)
+
+	robotURL := "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=68d83069-cde9-493f-9081-34537f132084"
+	postXCH(robotURL, xch)
 }
 
-func ReqNetspaceByURL(strURL string) {
-	resp, err := http.Get(strURL)
+func getJson(url string, target interface{}) error {
+	r, err := httpClient.Get(url)
 	if err != nil {
-		panic(err)
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+func postXCH(url string, xchInfo *XCH) string {
+
+	contents := fmt.Sprintf("今日币价简报:\n >全网算力: <font color=\"info\">%s EiB</font>\n>新增算力: <font color=\"comment\">%s PiB</font>\n>当前币价: <font color=\"warning\">%s USD</font>",
+		xchInfo.Netspace, xchInfo.Daychange, xchInfo.Price)
+
+	notify := new(Notify)
+	notify.Msgtype = "markdown"
+	notify.Markdown.Content = contents
+
+	fmt.Println(notify)
+
+	postBody, _ := json.Marshal(notify)
+	respBody := bytes.NewBuffer(postBody)
+	resp, err := http.Post(url, "application/json", respBody)
+	if err != nil {
+		log.Fatalf("Post failed: %v", err)
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("Response status:", resp.Status)
-
-	scanner := bufio.NewScanner(resp.Body)
-
-	for i := 0; scanner.Scan() && i < 5; i++ {
-		fmt.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
-}
-
-func ReqMarketByURL(strURL string) {
-	resp, err := http.Get(strURL)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
-	defer resp.Body.Close()
-
-	fmt.Println("Response status:", resp.Status)
-
-	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan() && i < 5; i++ {
-		fmt.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
+	sb := string(body)
+	log.Printf(sb)
+	return sb
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module xchrobot
 
 go 1.16
+
+require code.cloudfoundry.org/bytefmt v0.0.0-20210608160410-67692ebc98de
