62eaf7d7c86f6269e0f7beb463e65b0df9c4c936
shibowe <dev@winecos.com>
2021-06-27 16:38:54 +0800

    nothing

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/robfig/cron/v3"
 )
 
 type NetSpace struct {
@@ -41,11 +43,11 @@ var httpClient = &http.Client{Timeout: 10 * time.Second}
 func main() {
 	crontab := cron.New()
 	task := func() { handlerPost() }
-	spec := "0 19 * * *"
+	//spec := "0 19 * * *"
+	spec := "5 * * * *"
 	crontab.AddFunc(spec, task)
-
-	go crontab.Start()
-	defer crontab.Stop()
+	crontab.Start()
+	crontab.Stop()
 }
 func handlerPost() {
 	ns := new(NetSpace)
@@ -78,8 +80,8 @@ func handlerPost() {
 	xch.Netspace = fmt.Sprintf("%v", AllPower)
 	xch.Price = fmt.Sprintf("%v", XCHPrice)
 
-	//robotURL := "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=68d83069-cde9-493f-9081-34537f132084"
-	robotURL := "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=3e46f7e1-8c0a-4cd8-acbb-4c8a312ac7e5"
+	robotURL := "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=68d83069-cde9-493f-9081-34537f132084"
+	//robotURL := "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=3e46f7e1-8c0a-4cd8-acbb-4c8a312ac7e5"
 	postXCH(robotURL, xch)
 	fmt.Println("Push XCHPrice Success!")
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module xchrobot
 
 go 1.16
 
-require (
-	github.com/robfig/cron/v3 v3.0.1
-	github.com/thinkeridea/go-extend v1.3.2
-)
+require github.com/robfig/cron/v3 v3.0.1 // indirect
